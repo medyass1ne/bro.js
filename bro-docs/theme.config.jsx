29d623e40bba658a1f97bb955155f6fb6200107f
medyass1ne <mohyazzay@gmail.com>
2026-09-11 15:04:15 +0100

    #

--- bro-docs/theme.config.jsx
+++ bro-docs/theme.config.jsx
@@ -1,9 +1,46 @@
 import { useConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
 
 export default {
   logo: <img src="/bro.js-124-nobg.png" alt="bro.js Logo" width="64" height="64" />,
   head: () => {
     const { frontMatter } = useConfig();
+    const { asPath } = useRouter();
+
+    const pathWithoutQuery = asPath.split('?')[0].replace(/\/$/, '');
+    const segments = pathWithoutQuery.split('/').filter(Boolean);
+    
+    const itemListElement = [
+      {
+        "@type": "ListItem",
+        "position": 1,
+        "name": "Home",
+        "item": "https://brojs.yessindevs.me"
+      }
+    ];
+
+    let currentUrl = "https://brojs.yessindevs.me";
+    segments.forEach((segment, index) => {
+      currentUrl += `/${segment}`;
+      const name = segment
+        .split('-')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
+
+      itemListElement.push({
+        "@type": "ListItem",
+        "position": index + 2,
+        "name": name,
+        "item": currentUrl
+      });
+    });
+
+    const breadcrumbSchema = {
+      "@context": "https://schema.org",
+      "@type": "BreadcrumbList",
+      "itemListElement": itemListElement
+    };
+
     return (
       <>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -25,6 +62,12 @@ export default {
               "name": "bro.js",
               "url": "https://brojs.yessindevs.me"
             })
+          }}
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(breadcrumbSchema)
           }}
         />
       </>
